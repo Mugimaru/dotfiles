fa735df3276f003718db2a68b6cbf8394b2a1125
n.babushkin <n.babushkin@fun-box.ru>
2017-02-28 18:42:42 +0300

    fix installation order

--- install/asdf.sh
+++ install/asdf.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+ln -s $DOTFILES/asdf/dotfile $HOME/.asdfrc
+
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.2.1
+
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 
-ln -s $DOTFILES/asdf/dotfile $HOME/.asdfrc
