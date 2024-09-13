435f9d8703c9f0d7438cc4889df753c497cf2ee2
mugimaru <pochi.73@gmail.com>
2024-09-13 22:53:34 +0300

    update dotfiles

--- install/asdf.sh
+++ install/asdf.sh
@@ -2,12 +2,16 @@
 
 ln -s $DOTFILES/asdf/dotfile $HOME/.asdfrc
 
-git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.2.1
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.14.1
+git clone https://github.com/zplug/zplug $DOTFILES/zsh/zplug
 
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
 
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-
+asdf plugin add python
+asdf plugin add golang
+asdf plugin add erlang
+asdf plugin add elixir
+asdf plugin add ruby
+asdf plugin add rust
+asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
